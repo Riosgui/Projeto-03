e7cdf1b5513593de5faddc20d663ac51de8a73dd
Riosgui <guilhermerios100@hotmail.com>
2022-05-02 13:51:54 -0300

    V 1.0

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,13 @@
+import * as Styled from './styles';
+
+function Home() {
+  return (
+    <div className="App">
+      <Styled.Wrapper background="yellow" fontSize="48pt">
+        <h1>Hello</h1>
+      </Styled.Wrapper>
+    </div>
+  );
+}
+
+export default Home;
